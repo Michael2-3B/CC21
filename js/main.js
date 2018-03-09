1dd4ea840ee0f6c87b4d624569db8723082eefd6
Michael2-3B <34347626+Michael2-3B@users.noreply.github.com>
2018-03-08 23:16:20 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 var counter = document.getElementById("counter");
 
-counter.innerHTML = Math.PI;
+//This function doesn't work, due to the way javascript handles PI and numbers in general
+//Need an actual algorithm rather than using Math.PI
+function getDigit(i){
+  var result;
+  if(i==1)
+    result = 3;
+  else
+    result = parseInt(Math.PI * (10 ** (i-1))) - parseInt(Math.PI * (10 ** (i-2))) * 10;
+  return result;
+}
